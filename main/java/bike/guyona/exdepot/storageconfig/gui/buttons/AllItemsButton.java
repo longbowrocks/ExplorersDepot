30a28c543519767fc9ff4e56eb4eef9c332950e6
cmercer <longbowmail@yahoo.com>
2018-02-02 22:48:46 -0800

    created allItems toggle and clear button

--- main/java/bike/guyona/exdepot/storageconfig/gui/buttons/AllItemsButton.java
+++ main/java/bike/guyona/exdepot/storageconfig/gui/buttons/AllItemsButton.java
@@ -1,7 +1,11 @@
 package bike.guyona.exdepot.storageconfig.gui.buttons;
 
+import bike.guyona.exdepot.storageconfig.capability.StorageConfig;
+import bike.guyona.exdepot.storageconfig.gui.StorageConfigGui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+
+import static bike.guyona.exdepot.ExDepotMod.LOGGER;
 
 public class AllItemsButton extends GuiButton {
     public AllItemsButton(int buttonId, int x, int y, String buttonText) {
@@ -11,6 +15,16 @@ public class AllItemsButton extends GuiButton {
     @Override
     public boolean mousePressed(Minecraft mc, int i, int j) {
         if (super.mousePressed(mc, i, j)) {
+            if(mc.world != null && mc.player != null) {
+                if(mc.currentScreen != null && mc.currentScreen instanceof StorageConfigGui) {
+                    StorageConfigGui confGui = (StorageConfigGui) mc.currentScreen;
+                    StorageConfig conf = confGui.getStorageConfig();
+                    conf.allItems = !conf.allItems;
+                    confGui.setStorageConfig(conf);
+                } else {
+                    LOGGER.error("allitems screen is "+(mc.currentScreen == null ? "NULL" : mc.currentScreen.toString()));
+                }
+            }
             return true;
         }else {
             return false;
--- main/java/bike/guyona/exdepot/storageconfig/gui/buttons/ClearButton.java
+++ main/java/bike/guyona/exdepot/storageconfig/gui/buttons/ClearButton.java
@@ -1,7 +1,11 @@
 package bike.guyona.exdepot.storageconfig.gui.buttons;
 
+import bike.guyona.exdepot.storageconfig.capability.StorageConfig;
+import bike.guyona.exdepot.storageconfig.gui.StorageConfigGui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+
+import static bike.guyona.exdepot.ExDepotMod.LOGGER;
 
 public class ClearButton extends GuiButton {
     public ClearButton(int buttonId, int x, int y, String buttonText) {
@@ -11,6 +15,14 @@ public class ClearButton extends GuiButton {
     @Override
     public boolean mousePressed(Minecraft mc, int i, int j) {
         if (super.mousePressed(mc, i, j)) {
+            if(mc.world != null && mc.player != null) {
+                if(mc.currentScreen != null && mc.currentScreen instanceof StorageConfigGui) {
+                    StorageConfigGui confGui = (StorageConfigGui) mc.currentScreen;
+                    confGui.setStorageConfig(new StorageConfig());
+                } else {
+                    LOGGER.error("clear screen is "+(mc.currentScreen == null ? "NULL" : mc.currentScreen.toString()));
+                }
+            }
             return true;
         }else {
             return false;
