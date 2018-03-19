e3ed30997b7e778359881a172ccc9feee4cba6d3
cmercer <longbowmail@yahoo.com>
2018-03-18 23:01:47 -0700

    now I only need to change version in one place.

--- src/main/java/bike/guyona/exdepot/Ref.java
+++ src/main/java/bike/guyona/exdepot/Ref.java
@@ -7,7 +7,7 @@ public class Ref {
     public static final String MODID = "exdepot";
     public static final String NAME = "Explorer's Depot: A Sorting Utility";
     public static final String SHORT_NAME = "Explorer's Depot";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "TEST_VERSION_REPLACE";
     public static final String CLIENT_PROXY = "bike.guyona.exdepot.proxy.ClientProxy";
     public static final String SERVER_PROXY = "bike.guyona.exdepot.proxy.CommonProxy";
 
