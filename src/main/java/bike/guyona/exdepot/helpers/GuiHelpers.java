14765d69bf6e86084b1bec8819b9060f293491f3
cmercer <longbowmail@yahoo.com>
2018-05-25 09:22:23 -0700

    fix memory leak

--- src/main/java/bike/guyona/exdepot/helpers/GuiHelpers.java
+++ src/main/java/bike/guyona/exdepot/helpers/GuiHelpers.java
@@ -3,7 +3,6 @@ package bike.guyona.exdepot.helpers;
 import bike.guyona.exdepot.gui.interfaces.IHasTooltip;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderItem;
@@ -19,18 +18,14 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.client.config.GuiUtils;
 import net.minecraftforge.fml.common.ModContainer;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Vector;
 
 import static bike.guyona.exdepot.ExDepotMod.LOGGER;
 
@@ -84,14 +79,23 @@ public class GuiHelpers {
         return logo;
     }
 
+    private static HashMap<String, ResourceLocation> modLogosCache;
+
     public static void drawMod(int left, int top, float zLevel, ModContainer mod, int width, int height) {
+        if (modLogosCache == null) {
+            modLogosCache = new HashMap<>();
+        }
         BufferedImage logo = getModLogo(mod);
         if (logo != null)
         {
             Tessellator tess = Tessellator.getInstance();
             Minecraft mc = Minecraft.getMinecraft();
             TextureManager tm = mc.getTextureManager();
-            ResourceLocation logoPath = tm.getDynamicTextureLocation("modlogo", new DynamicTexture(logo));
+            ResourceLocation logoPath = modLogosCache.get(mod.getModId());
+            if (logoPath == null) {
+                logoPath = tm.getDynamicTextureLocation("modlogo", new DynamicTexture(logo));
+                modLogosCache.put(mod.getModId(), logoPath);
+            }
             Dimension logoDims = new Dimension(width, height);
 
             GlStateManager.enableBlend();
